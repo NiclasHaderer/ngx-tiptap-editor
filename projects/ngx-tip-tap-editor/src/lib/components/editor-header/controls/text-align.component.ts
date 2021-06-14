a08d4cb0a26e7c5bab5a9c4aac3f9638fc536df1
Niclas Haderer <niclas.haderer.dev@gmail.com>
2021-06-14 10:45:45 +0200

    Moved the text align component to the new select base component

--- projects/ngx-tip-tap-editor/src/lib/components/editor-header/controls/text-align.component.ts
+++ projects/ngx-tip-tap-editor/src/lib/components/editor-header/controls/text-align.component.ts
@@ -1,11 +1,12 @@
 import { Component, forwardRef } from '@angular/core';
-import { BaseControl } from './base-control';
+import { TiptapEventService } from '../../../services/tiptap-event.service';
+import { BaseControl, SelectBaseControl } from './base-control';
 
 @Component({
   selector: 'tip-text-align-control',
   styleUrls: ['_styles.scss'],
   template: `
-    <tip-select [value]="getAlignment()" defaultValue="left" width="auto" [showIcon]="false"
+    <tip-select defaultValue="left" width="auto" [showIcon]="false"
                 (change)="setAlign($event)">
       <tip-option value="left" [useHtml]="true" [enforceHeight]="true">
         <i class="material-icons">format_align_left</i>
@@ -23,12 +24,25 @@ import { BaseControl } from './base-control';
   `,
   providers: [{provide: BaseControl, useExisting: forwardRef(() => TextAlignControlComponent)}],
 })
-export class TextAlignControlComponent extends BaseControl {
+export class TextAlignControlComponent extends SelectBaseControl {
+  protected canStyleParams = ['left', 'right', 'center', 'justify'];
+
+  constructor(
+    protected eventService: TiptapEventService
+  ) {
+    super();
+  }
+
   public setAlign(alignment: 'justify' | 'left' | 'center' | 'right'): void {
     this.editor && this.editor.chain().focus().setTextAlign(alignment).run();
   }
 
-  public getAlignment(): 'left' | 'right' | 'center' | 'justify' | null {
+  protected canStyle(alignment: 'justify' | 'left' | 'center' | 'right'): boolean {
+    if (!this.editor) return false;
+    return this.editor.can().setTextAlign(alignment);
+  }
+
+  protected currentActive(): 'left' | 'right' | 'center' | 'justify' | null {
     if (this.editor) {
       if (this.editor.isActive({textAlign: 'left'})) return 'left';
       if (this.editor.isActive({textAlign: 'right'})) return 'right';
